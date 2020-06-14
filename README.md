915e98171cfd01d0185d635d37b9c906dfcb30a7
PichotM3 <pichotcontact@gmail.com>
2020-06-14 13:18:34 -0400

    project: push early base

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fivem-redm-wrapper
-FiveM to RedM natives wrapper attempt
+FiveM to RedM natives wrapper.
+
+The goal of this project is to create a complete and working wrapper to be able to import FiveM scripts and gamemodes to RedM without having to make any modifications. We aim to bring a perfect compatibility and translate all different sub-systems between the two games. For instance, adapting the FiveM clothes/components systems to RedM or even how notifications work as these were highly modified.
+
+# Links
+- [FiveM](https://fivem.net/) is a singleplayer/multiplayer modification framework for GTA V.
+- [RedM](https://redm.gg/) is a singleplayer/multiplayer modification framework for RDR2.
+
+# Documentations
+Natives documentations are made by the modding community.
+- [GTA V](https://runtime.fivem.net/doc/natives/) native reference.
+- [RDR2](https://vespura.com/doc/natives/) native reference.
---
+++ redm-wrapper/client/PED.lua
@@ -0,0 +1,25 @@
+-- global loader bits
+local _i, _f, _v, _r, _ri, _rf, _rl, _s, _rv, _ro, _in, _ii, _fi =
+	Citizen.PointerValueInt(), Citizen.PointerValueFloat(), Citizen.PointerValueVector(),
+	Citizen.ReturnResultAnyway(), Citizen.ResultAsInteger(), Citizen.ResultAsFloat(), Citizen.ResultAsLong(), Citizen.ResultAsString(), Citizen.ResultAsVector(), Citizen.ResultAsObject(),
+	Citizen.InvokeNative, Citizen.PointerValueIntInitialized, Citizen.PointerValueFloatInitialized
+
+local g = _G
+local msgpack = msgpack
+local _tostring = tostring
+local type = type
+local function _ts(num)
+	if num == 0 or not num then -- workaround for users calling string parameters with '0', also nil being translated
+		return nil
+	end
+	return _tostring(num)
+end
+
+local formerGetPlayerPed = GetPlayerPed
+GetPlayerPed = function(pedId)
+	if pedId == -1 then
+		return PlayerPedId()
+	else
+		return formerGetPlayerPed(pedId)
+	end
+end
